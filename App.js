4f8a0b99de83c75fc039acb2d74f677e4d1c85db
Tav Hafner <tav.hafner@gmail.com>
2018-05-08 13:22:19 -0600

    maps are working

--- App.js
+++ App.js
@@ -7,48 +7,95 @@
 import React, { Component } from 'react';
 import {
   Text,
-  StyleSheet
+  StyleSheet,
+  Dimensions
 } from 'react-native';
 import MapView from 'react-native-maps';
 //import ClusteredMapView from 'react-native-maps-super-cluster';
 import image from './images/flag-pink.png';
 
+
+const { width, height } = Dimensions.get('window');
+
+
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
 export default class App extends Component<{}> {
+  constructor() {
+    super();
+
+    this.state = {
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0
+      }, 
+      markerPosition: {
+        latitude: 0,
+        longitude: 0
+      }
+    } 
+  }
+
+  watchID: ?number = null
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var lat = parseFloat(position.coords.latitude);
+      var long = parseFloat(position.coords.longitude);
+
+      var initialRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+
+      this.setState({
+        initialPosition: initialRegion,
+        markerPosition: initialRegion
+      })
+    }, (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+      this.watchID = navigator.geolocation.watchPosition((position) => {
+        var lat = parseFloat(position.coords.latitude);
+        var long = parseFloat(position.coords.longitude);
+
+        var lastRegion = {
+          latitude: lat,
+          longitude: long,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA
+        }
+
+        this.setState({
+          initialPosition: lastRegion,
+          markerPosition: lastRegion
+        })
+      })
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID); 
+  }
+
   render() {
 
-    const coordinates = [];
-
-    coordinates.push({
-      key: 0,
-      location: {
-        longitude: -70.23,
-        latitude: -33.23
-      }
-    });
-
-    for(let i = 1; i<100; i++) {
-
-      const location = {
-        longitude: coordinates[i-1].location.longitude + (Math.random() * (i%2 === 0 ? -1 : 1)),
-        latitude: coordinates[i-1].location.latitude + (Math.random() * (i%2 === 0 ? -1 : 1)),
-      };
-
-      coordinates.push({ key: i, location });
-
-    }
 
     return (
       <MapView
-        renderMarker={renderMarker}
-        initialRegion={{
-          longitude: -70.23,
-          latitude: -33.23,
-          latitudeDelta: 9.22,
-          longitudeDelta: 4.21,
-        }}
+        region={this.state.initialPosition}
         style={StyleSheet.absoluteFillObject}>
-
-        { coordinates.map(({ key, location } ) => <MapView.Marker key={key} image={image} coordinate={location} />) }
+        <MapView.Marker coordinate={this.state.markerPosition}>
+         
+        </MapView.Marker>
+        
 
       </MapView>
     );
